2699b944f373244e69000f34281a1b27203c484d
Steven Holder <stevenholder1@gmail.com>
2012-02-19 12:07:41 -0500

    typo

--- security.php
+++ security.php
@@ -2,7 +2,7 @@
 class Security {
 	public static function encrypt($input, $key) {
 		$size = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB); 
-		$input = $this->pkcs5_pad($input, $size); 
+		$input = Security::pkcs5_pad($input, $size); 
 		$td = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_ECB, ''); 
 		$iv = mcrypt_create_iv (mcrypt_enc_get_iv_size($td), MCRYPT_RAND); 
 		mcrypt_generic_init($td, $key, $iv); 

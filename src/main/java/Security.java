b1ddad886d62fccb9ec6e5a61ac165a4a49174c1
fishjd <fishjd@gmail.com>
2014-04-19 08:40:32 -0600

    Convert to Maven project. Correct the case on Security.java

---
+++ src/main/java/Security.java
@@ -0,0 +1,48 @@
+
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
+public class Security {
+
+	public static String encrypt(String input, String key) {
+		byte[] crypted = null;
+		try {
+			SecretKeySpec skey = new SecretKeySpec(key.getBytes(), "AES");
+			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+			cipher.init(Cipher.ENCRYPT_MODE, skey);
+			crypted = cipher.doFinal(input.getBytes());
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return new String(Base64.encodeBase64(crypted));
+	}
+
+	public static String decrypt(String input, String key) {
+		byte[] output = null;
+		try {
+			SecretKeySpec skey = new SecretKeySpec(key.getBytes(), "AES");
+			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+			cipher.init(Cipher.DECRYPT_MODE, skey);
+			output = cipher.doFinal(Base64.decodeBase64(input));
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return new String(output);
+	}
+
+	public static void main(String[] args) {
+		String key = "1234567891234567";
+		String data = "Sensitive Data";
+		System.out.println("Start = " + data);
+		String encrypt = Security.encrypt(data, key);
+		System.out.println("Encrypted = " + encrypt);
+		String decrypt = Security.decrypt(encrypt, key);
+		System.out.println("Decrypted = " + decrypt);
+		if (data.equals(decrypt)) {
+			System.out.println("Success");
+		}
+
+	}
+}
